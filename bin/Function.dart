9010f44ee6ba2c4f2acb381082c9ae14b5317933
91898 <chandansatyendraprasad@gmail.com>
2022-07-05 16:17:44 +0530

    Function

--- bin/Function.dart
+++ bin/Function.dart
@@ -1,25 +1,70 @@
 void main() {
-
   fun();
   fun1();
   var test = fun2(3, 10);
   print('Mul = $test');
-  sum(5,6);
+  sum(5, 6);
   factorial(5);
+  var a = [2, 3.5, 'cex', true];
+  funlist(a);
+
+  var rf = recur_fun(5);
+  print('Recursion Sum of N = ${rf}');
+
+  //through Exception
+  try {
+    info();
+  }
+  catch(e)
+  {
+    print('object = $e');
+  }
+
+  // Lambda Expression
+  lam1();
+  var tg=lam2();
+  print('Lambda Expression = $tg');
+}
+
+int lam2() => 5+9;
+
+void lam1() => print(3+4);
+
+void info() {
+  for (var i = 0; i <= 10; i++) {
+    if (i == 5) {
+      throw FormatException;
+    }
+    print('object i=$i');
+  }
+}
+
+// Recursion
+int recur_fun(int i) {
+  if (i != 0) {
+    return i + recur_fun(i - 1);
+  } else {
+    return 0;
+  }
+}
+
+void funlist(List<Object> a) {
+  for (var i = 0; i < a.length; i++) {
+    print('Value ${i + 1} = ${a[i]}');
+  }
 }
 
 void factorial(int i) {
-  var fact=1;
-  while(i!=0)
-    {
-      fact=fact*i;
-      i--;
-    }
-    print('Factorial = $fact');
+  var fact = 1;
+  while (i != 0) {
+    fact = fact * i;
+    i--;
+  }
+  print('Factorial = $fact');
 }
 
 void sum(int i, int j) {
-  print('Sum = ${i+j}');
+  print('Sum = ${i + j}');
 }
 
 fun2(int i, int j) {
